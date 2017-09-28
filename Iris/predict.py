2ab77a68c57b8b3475f727f73e4488c29607bad8
yennanliue <f339339@gmail.com>
2017-09-28 10:25:28 +0100

    fix tree model to tree-classifier and add multiple model train function

--- Iris/predict.py
+++ Iris/predict.py
@@ -24,9 +24,14 @@ def load_data():
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import linear_model
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
 
 
 def train(df,model):
@@ -38,19 +43,65 @@ def train(df,model):
     print ('test score :', model.score(X_test,y_test) )
     y_true = y_test
     y_pred = clf_.predict(X_test)
+    print ('-------------------')
+    print ('confusion matrix : ')
     print (confusion_matrix(y_true, y_pred))
+    print ('-------------------')
+    print ('classification report  : ')
+    print (classification_report(y_true, y_pred))
     #print (clf_)
     return clf_
 
-def predict(model, **kwargs):
-	pass 
 
-def test_():
+def train_multiple_model():
+	from sklearn import model_selection
+	from sklearn.linear_model import LogisticRegression
+	from sklearn.neighbors import KNeighborsClassifier
+	from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+	from sklearn.naive_bayes import GaussianNB
+	from sklearn.svm import SVC
+	seed = 7
+	scoring = 'accuracy'
 	df_iris = load_data()
-	clf_tree = tree.DecisionTreeRegressor()
-	train(df_iris,clf_tree)
-	print (sys.argv[1:])
-	print (clf_tree.predict(sys.argv[1:]))
+	df_iris_ = df_iris.copy()
+	X = df_iris_.iloc[:,1:5]
+	y = df_iris_.iloc[:,6:7]
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+	models = []
+	models.append(('LR', LogisticRegression()))
+	models.append(('LDA', LinearDiscriminantAnalysis()))
+	models.append(('KNN', KNeighborsClassifier()))
+	models.append(('CART', DecisionTreeClassifier()))
+	models.append(('NB', GaussianNB()))
+	models.append(('SVM', SVC()))
+	# evaluate each model in turn
+	results = []
+	names = []
+	for name, model in models:
+		kfold = model_selection.KFold(n_splits=10, random_state=seed)
+		cv_results = model_selection.cross_val_score(model, X_train,y_train, cv=kfold, scoring=scoring)
+		results.append(cv_results)
+		names.append(name)
+		msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+		print ('-------------------')
+		print(msg)
+		print ('-------------------')
+
+
+#def predict_(model, **kwargs):
+#	pass 
+
+def predict():
+	if len(sys.argv[1:]) != 4:
+		print ('plz enter iris parameters for species peediction with form : "SepalLengthCm"  "SepalWidthCm" "PetalLengthCm" "PetalWidthCm"')
+		# e.g. python predict.py 1 2 3 4 
+		# output : 
+	else:
+		df_iris = load_data()
+		clf_tree = DecisionTreeClassifier()
+		train(df_iris,clf_tree)
+		print (sys.argv[1:])
+		print (clf_tree.predict(sys.argv[1:]))
 
 #def test():
 #	#for arg in sys.argv: 
@@ -58,9 +109,11 @@ def test_():
 #	print (sys.argv[1:])
 
 
+
+
 if __name__ == '__main__':
 
-	test_()
+	predict()
 	"""
 	clf_tree = tree.DecisionTreeRegressor()
 	clf_rf = RandomForestClassifier()
@@ -70,6 +123,7 @@ if __name__ == '__main__':
 	for model in [clf_tree,clf_rf,clf_logistic]:
 		model = train(df_iris,model )
 	"""
+	#train_multiple_model()
 
 
 
