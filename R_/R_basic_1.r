d408edb532cf690e6a58d92c0f890f66c031baa0
yennanliue <f339339@gmail.com>
2017-09-30 20:40:00 +0100

    start playing R

---
+++ R_/R_basic_1.r
@@ -0,0 +1,73 @@
+
+# show all file list in the working directory
+#dir()
+
+#get route of current directory
+#getwd()
+
+df_test <- read.csv(file="/Users/yennanliu/Desktop/dev_/df_test.csv",head=TRUE,sep=",")
+
+df_test
+
+summary(df_test)
+
+df_test$B
+
+df_test$X
+
+names(df_test)
+
+attributes(df_test)
+
+names(df_test)
+
+
+
+a <- 3
+
+a
+
+typeof(a)
+
+b <- sqrt(a*a+3)
+
+b
+
+ls()
+
+a <- c(1,2,3,4,5)
+
+a
+
+a+1
+
+mean(a)
+var(a)
+
+a[1]
+a[2]
+
+a <- numeric(10)
+
+a
+
+a <- "hello"
+
+typeof(a)
+
+a
+
+b <- c("hello","there")
+
+b
+b[0]
+b[1]
+b[2]
+
+typeof(b)
+
+a = character(20)
+
+a
+
+
