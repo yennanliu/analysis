c46dfdb8a9a0ea813b63d6f2cba01d6adc49a2de
yennanliue <f339339@gmail.com>
2017-10-02 09:00:24 +0100

    modify data prepare process

--- zyp_/data_prepare.py
+++ zyp_/data_prepare.py
@@ -15,6 +15,14 @@ def load_data():
 	df_UGC_count.columns = ['USER', 'UGC_COUNT', 'Tweets', 'Stories', 'IG_Posts', 'Other',
        'Unnamed', 'USER2', 'UGC_COUNT2', 'Tweets2', 'Stories2',
        'IG_Posts2']
+
+	return df_directory, df_UGC_count
+
+def data_prepare():
+	df_directory, df_UGC_count = load_data()
+	df_directory['Insta_link'] = df_directory.Insta_link.map(lambda x : fix_insta_line(x))
+	df_directory['Insta_id'] = df_directory['Insta_link'].map(lambda x : get_insta_id(x))
+	print (df_directory)
 	df_UGC_count1 = df_UGC_count[['USER', 'UGC_COUNT', 'Tweets', 'Stories', 'IG_Posts', 'Other']]
 	df_UGC_count2 = df_UGC_count[['USER2', 'UGC_COUNT2', 'Tweets2', 'Stories2',
        'IG_Posts2']]
@@ -22,14 +30,7 @@ def load_data():
 	df_UGC_count2 = df_UGC_count2.dropna(how='all')
 	print (df_UGC_count1.head())
 	print (df_UGC_count2.head())
-	return df_directory, df_UGC_count1, df_UGC_count2, df_UGC_count
-
-def data_prepare():
-	df_directory, df_UGC_count1, df_UGC_count2, df_UGC_count = load_data()
-	df_directory['Insta_link'] = df_directory.Insta_link.map(lambda x : fix_insta_line(x))
-	df_directory['Insta_id'] = df_directory['Insta_link'].map(lambda x : get_insta_id(x))
-	print (df_directory)
-	return df_directory
+	return df_directory, df_UGC_count1, df_UGC_count2 
 
 
 
