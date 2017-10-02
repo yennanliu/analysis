97726a8a250ceeac7f6dcc1ee1a5dafb64013bc0
yennanliue <f339339@gmail.com>
2017-10-02 08:51:20 +0100

    add df_UGC_count data prepare code

--- zyp_/data_prepare.py
+++ zyp_/data_prepare.py
@@ -6,13 +6,26 @@ import numpy as np
 
 
 def load_data():
+	# load directory.csv 
 	df_directory = pd.read_csv('directory.csv',thousands=r',')
 	df_directory.columns= ['No','DMed', 'Type', 'Insta_link', 'Followers', 'Address','Unnamed']
 	df_directory['Insta_link'] = df_directory['Insta_link'].astype('str')
-	return df_directory
+	# load UGC_count.csv
+	df_UGC_count = pd.read_csv('UGC_count.csv')
+	df_UGC_count.columns = ['USER', 'UGC_COUNT', 'Tweets', 'Stories', 'IG_Posts', 'Other',
+       'Unnamed', 'USER2', 'UGC_COUNT2', 'Tweets2', 'Stories2',
+       'IG_Posts2']
+	df_UGC_count1 = df_UGC_count[['USER', 'UGC_COUNT', 'Tweets', 'Stories', 'IG_Posts', 'Other']]
+	df_UGC_count2 = df_UGC_count[['USER2', 'UGC_COUNT2', 'Tweets2', 'Stories2',
+       'IG_Posts2']]
+	df_UGC_count1 = df_UGC_count1.dropna(how='all')
+	df_UGC_count2 = df_UGC_count2.dropna(how='all')
+	print (df_UGC_count1.head())
+	print (df_UGC_count2.head())
+	return df_directory, df_UGC_count1, df_UGC_count2, df_UGC_count
 
-def directory_data_prepare():
-	df_directory = load_data()
+def data_prepare():
+	df_directory, df_UGC_count1, df_UGC_count2, df_UGC_count = load_data()
 	df_directory['Insta_link'] = df_directory.Insta_link.map(lambda x : fix_insta_line(x))
 	df_directory['Insta_id'] = df_directory['Insta_link'].map(lambda x : get_insta_id(x))
 	print (df_directory)
@@ -37,7 +50,7 @@ def get_insta_id(x):
 
 
 if __name__ == '__main__':
-	directory_data_prepare()
+	data_prepare()
 
 
 
