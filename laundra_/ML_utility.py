6cde2745cc7bf1efaa51d71e13301b564ed5717f
yennanliue <f339339@gmail.com>
2017-10-19 17:15:42 +0100

    add k-fold train method

--- laundra_/ML_utility.py
+++ laundra_/ML_utility.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import GridSearchCV, learning_curve
 from sklearn import preprocessing, model_selection, metrics, feature_selection
 from sklearn import grid_search
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 
 class Class_Fit(object):
@@ -29,6 +30,31 @@ class Class_Fit(object):
     def grid_predict(self, X, Y):
         self.predictions = self.grid.predict(X)
         print("Precision: {:.2f} % ".format(100*metrics.accuracy_score(Y, self.predictions)))
+
+
+class Class_train(object):
+
+    def __init__(self, clf, params=None):
+        if params:            
+            self.clf = clf(**params)
+        else:
+            self.clf = clf()
+
+    def simple_train(self, clf, x_train,y_train):
+        self.clf.fit(x_train,y_train)
+
+    def k_fold_train(self, KFold_ ,x_train, y_train):
+        xtrain_ = x_train.values
+        ytrain_ = y_train.values
+        for i, (train_ind, val_ind) in enumerate(KFold(n_splits=KFold_, shuffle=True, 
+                                        random_state=1989).split(xtrain_)):
+
+            print('----------------------')
+            print('Training model #%d' % i)
+            print('----------------------')
+            self.clf.fit(xtrain_[train_ind], ytrain_[train_ind])
+            print (self.clf.score(xtrain_[val_ind],ytrain_[val_ind]))
+
 
 
 
