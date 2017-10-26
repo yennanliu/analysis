23acf43d6581335a9735d3c633d45faec62a3c93
yennanliue <f339339@gmail.com>
2017-10-27 00:20:51 +0100

    backup initial setup script

---
+++ laundra_/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+echo 'plz make sure you are using python 3 '
+# install python 3 env 
+echo 'install needed packages....'
+pip install pandas numpy 
+
+
+# make sure needed data exist (csv)
+
+
+# create needed files (if no exist)
