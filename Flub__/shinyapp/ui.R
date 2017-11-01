3726f4342c6670f97ab161d46ea16c6cc23c9493
yennanliue <f339339@gmail.com>
2017-11-01 16:31:44 +0000

    refactor simple R shiny application

--- Flub__/shinyapp/ui.R
+++ Flub__/shinyapp/ui.R
@@ -1,14 +1,24 @@
 
 
-library(shiny)
-
-# Define UI for miles per gallon application
 shinyUI(pageWithSidebar(
-
-  # Application title
-  headerPanel("Miles Per Gallon"),
-
-  sidebarPanel(),
-
-  mainPanel()
+  headerPanel("CSV Viewer"),
+  sidebarPanel(
+    fileInput('file1', 'Choose CSV File',
+              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+    tags$hr(),
+    checkboxInput('header', 'Header', TRUE),
+    radioButtons('sep', 'Separator',
+                 c(Comma=',',
+                   Semicolon=';',
+                   Tab='\t'),
+                 'Comma'),
+    radioButtons('quote', 'Quote',
+                 c(None='',
+                   'Double Quote'='"',
+                   'Single Quote'="'"),
+                 'Double Quote')
+  ),
+  mainPanel(
+    tableOutput('contents')
+  )
 ))
