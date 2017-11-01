798b6a8b76f3c50c5379dfa09b98db69231be9d9
yennanliue <f339339@gmail.com>
2017-11-01 15:07:54 +0000

    data prepare for R shiny

---
+++ Flub__/data_prepare_R.py
@@ -0,0 +1,23 @@
+
+# python 3 
+# analysis 
+import pandas as pd 
+
+
+def parse_csv():
+	csv_route='/Users/yennanliu/analysis/Flub__'
+	# only load few columns 
+	# since there are potential data issues may affect column name mismatch it data 
+	df_order = pd.read_csv(csv_route + '/orders.csv',usecols=["order_item_id",
+	                                                          "order_id",
+	                                                          "created_at",
+	                                                          "order_status"])
+	print (df_order.head())
+	df_order.to_csv('order_fix_.csv')
+	return df_order
+
+
+
+
+if __name__ == '__main__':
+	parse_csv()
