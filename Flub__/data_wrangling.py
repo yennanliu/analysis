af8dd4fc3bd4a89dacc6a4fb519e83a27de88766
yennanliue <f339339@gmail.com>
2017-11-01 12:12:11 +0000

    fix nan data type and finish simple analysis

--- Flub__/data_wrangling.py
+++ Flub__/data_wrangling.py
@@ -30,18 +30,46 @@ def data_preprocess():
 		try:
 			stock_.append(d['products'][k]['merchants'][0]['stock'])
 		except:
-			stock_.append('NaN')
+			stock_.append(float('nan'))
 			pass
 		    
 	df_product_match['stock'] =  stock_
+	# fix type 
+	df_product_match.score = df_product_match.score.convert_objects(convert_numeric=True)
+	df_product_match.stock = df_product_match.stock.convert_objects(convert_numeric=True)
+	# show the data 
 	print (df_product_match.head())
 	return df_product_match
 
 
+def analysis():
+	df_product_match = data_preprocess()
+	print ('')
+	print ('1)  General Situation --------------')
+	# all
+	print ('all data counts : ', len(df_product_match))
+
+	# accepted 
+	print ('accepted data counts : ', len(df_product_match[df_product_match.score > 0.5]))
+
+	# rejected
+	print ('rejected data counts : ', len(df_product_match[df_product_match.score < 0.02]))
+
+	# should be checked by agents
+	print ('should be checked by agents data counts : ', len(df_product_match[(df_product_match.score <= 0.5)
+	                            & (df_product_match.score >= 0.02) ]))
+	print ('')
+	print ('')
+	print ('2) Considering only the accepted products --------------')
+	print ('accepted & in stock product : ',len(df_product_match[(df_product_match.score > 0.5)
+                 & (df_product_match.stock > 0)]))
+ 
+
 
 
 if __name__ == '__main__':
-	data_preprocess()
+	#data_preprocess()
+	analysis()
 
 
 
