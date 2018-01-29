2efd0d690536889ce3abc1659ec9fd3a116f1b36
yennanliue <f339339@gmail.com>
2018-01-29 02:13:11 +0000

    add heatmap plot function

--- utility_plot.py
+++ utility_plot.py
@@ -4,6 +4,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
+import folium
+from folium import plugins
 
 # Display plots inline and change default figure size
 %matplotlib inline
@@ -40,6 +42,21 @@ def plot_decision_boundary(pred_func):
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
 
 
+
+
+
+def plot_heatmap_lon_lat(df,map_center):
+    #map_center = [50.87650,4.39723]
+    print (map_center)
+    heatmap = folium.Map(location=map_center, 
+                         tiles='https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ29nb3ZhbmFwcCIsImEiOiJjaWg3MTk0ZjAwZmI5dXVraTF5aDNudTd0In0.mcuRTlI-EWoMm0IponVnVA',
+                         attr='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                         zoom_start=11)
+
+    # plz modify lon / lat columns here in cases for various dataset 
+    heatmap_locations_list = df[['start_lat', 'start_lon']].dropna().values.tolist()
+    heatmap.add_children(plugins.HeatMap(heatmap_locations_list, radius=20, blur=30))
+    display(heatmap)
 
 
 
