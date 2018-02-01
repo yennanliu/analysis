fc60e31774e20df0810167a02cb04f54528b125f
yennanliue <f339339@gmail.com>
2018-02-01 09:54:35 +0000

    backup

--- utility_plot.py
+++ utility_plot.py
@@ -1,5 +1,10 @@
 # python 3 
 
+# collection of all plotting functions for data visualzation in python 
+
+
+
+
 # Package imports
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,10 +12,10 @@ import matplotlib
 import folium
 from folium import plugins
 
+
 # Display plots inline and change default figure size
 %matplotlib inline
 matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
-
 
 
 
@@ -57,6 +62,127 @@ def plot_heatmap_lon_lat(df,map_center):
     heatmap_locations_list = df[['start_lat', 'start_lon']].dropna().values.tolist()
     heatmap.add_children(plugins.HeatMap(heatmap_locations_list, radius=20, blur=30))
     display(heatmap)
+
+
+"""
+credit 
+
+http://nbviewer.jupyter.org/github/python-visualization/folium/blob/master/examples/Plugins.ipynb
+https://www.kaggle.com/daveianhickey/how-to-folium-for-maps-heatmaps-time-analysis
+https://github.com/python-visualization/folium/issues/441
+
+"""
+
+def plot_dynamic_heatmap_lon_lat(df):
+
+    m = folium.Map(
+        location=[35.68159659061569, 139.76451516151428],
+        zoom_start=16
+    )
+
+    # Lon, Lat order.
+    """
+    to fix here 
+    
+    lines_ = [{'coordinates': json.loads(json.dumps(df_[df_.booking_start_day == k][['start_lat','start_lon']].values.tolist())),
+          'dates': json.loads(json.dumps(df_[df_.booking_start_day == k][['booking_start_date']].values.tolist())),
+          'color': 'red' } for k in set(df_.booking_start_day) 
+        ]
+    """
+
+    lines = [
+        {
+            'coordinates': [
+                [139.76451516151428, 35.68159659061569],
+                [139.75964426994324, 35.682590062684206],
+            ],
+            'dates': [
+                '2017-06-02T00:00:00',
+                '2017-06-02T00:10:00'
+            ],
+            'color': 'red'
+        },
+        {
+            'coordinates': [
+                [139.75964426994324, 35.682590062684206],
+                [139.7575843334198, 35.679505030038506],
+            ],
+            'dates': [
+                '2017-06-02T00:10:00',
+                '2017-06-02T00:20:00'
+            ],
+            'color': 'blue'
+        },
+        {
+            'coordinates': [
+                [139.7575843334198, 35.679505030038506],
+                [139.76337790489197, 35.678040905014065],
+            ],
+            'dates': [
+                '2017-06-02T00:20:00',
+                '2017-06-02T00:30:00'
+            ],
+            'color': 'green',
+            'weight': 15,
+        },
+        {
+            'coordinates': [
+                [139.76337790489197, 35.678040905014065],
+                [139.76451516151428, 35.68159659061569],
+            ],
+            'dates': [
+                '2017-06-02T00:30:00',
+                '2017-06-02T00:40:00'
+            ],
+            'color': '#FFFFFF',
+        },
+    ]
+
+
+    features = [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'LineString',
+                'coordinates': line['coordinates'],
+            },
+            'properties': {
+                'times': line['dates'],
+                'style': {
+                    'color': line['color'],
+                    'weight': line['weight'] if 'weight' in line else 5
+                }
+            }
+        }
+        for line in lines
+        
+    ]
+
+
+    plugins.TimestampedGeoJson({
+        'type': 'FeatureCollection',
+        'features': features,
+    }, period='PT1M', add_last_point=True).add_to(m)
+
+
+    #m.save(os.path.join('results', 'Plugins_5.html'))
+
+    m 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
