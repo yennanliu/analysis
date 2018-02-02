17fe12e1e7cc1720fbb2b0af0994dae43bf2df63
yennanliue <f339339@gmail.com>
2018-02-02 09:05:17 +0000

    add trip sql and data preprocess function

--- z_demand_predict/run_test.py
+++ z_demand_predict/run_test.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from pytz import timezone
 import datetime
 import os
+import math 
 
 # UDF 
 from grab_from_db import *
@@ -14,17 +15,31 @@ from sql_collect import *
 
 
 def get_data():
-	sql_test= sql['sql_test']
+	sql_test= sql['sql_trips']
 	get_data_from_db = work_with_db(db_url)
 	df_ = get_data_from_db.read_from_db(sql_test)
 	print (df_)
 	return df_
 
 
-
 def preprocess(df):
-	pass 
-
+	df_ = df.copy()
+	df_['trip_start_date'] = pd.to_datetime(df_.trip_start_date)
+	df_['trip_start_day'] = pd.to_datetime(df_.trip_start_day)
+	df_.loc[:, 'tripstart_day'] = df_['trip_start_date'].dt.day
+	df_.loc[:, 'tripstart_weekday'] = df_['trip_start_date'].dt.weekday
+	df_.loc[:, 'tripstart_month'] = df_['trip_start_date'].dt.month
+	df_.loc[:, 'tripstart_year'] = df_['trip_start_date'].dt.year 
+	df_.loc[:, 'tripstart_hour'] = df_['trip_start_date'].dt.hour
+	df_.loc[:, 'tripstart_minute'] = df_['trip_start_date'].dt.minute
+	df_.loc[:,'dayofyear'] = df_['trip_start_day'].apply(lambda x: x.dayofyear)
+	df_['month_num'] = (df_['tripstart_day']-1)/30.
+	df_['year_sin'] = (df_['dayofyear'] * 2 * math.pi).apply(math.sin)
+	df_['year_cos'] = (df_['dayofyear'] * 2 * math.pi).apply(math.cos)
+	df_['month_sin'] = (df_['month_num'] * 2 * math.pi).apply(math.sin)
+	df_['month_cos'] = (df_['month_num'] * 2 * math.pi).apply(math.cos)
+	print (df_)
+	return df_
 
 
 
@@ -40,7 +55,8 @@ def preprocess(df):
 
 
 if __name__ == '__main__':
-	get_data()
+	df = get_data()
+	preprocess(df)
 
 
 
--- z_demand_predict/sql_collect.py
+++ z_demand_predict/sql_collect.py
@@ -4,4 +4,41 @@
 sql = {'sql_test' : """
 		SELECT * FROM rw.blue_move
 		limit 10 
-		"""}
+		""",
+	  'sql_trips':"""
+
+		  WITH fleet_team_id AS
+	  (SELECT member_id
+	   FROM ana.members
+	   WHERE ROLE != 'customer' )
+	SELECT t.booking_start_date,
+	       t.trip_start_date,
+	       date(t.booking_start_date) AS book_start_day,
+	       date(t.trip_start_date) AS trip_start_day,
+	       t.vin,
+	       t.member_id,
+	       sz.name AS start_zone_name,
+	       ez.name AS end_zone_name,
+	       t.start_lat,
+	       t.start_lon,
+	       t.end_lat,
+	       t.end_lon,
+	       t.distance,
+	       t.trip_duration
+	FROM prc.trips t
+	LEFT JOIN rw.quartiers sz ON st_contains(sz.geom, t.start_pos_gis)
+	LEFT JOIN rw.quartiers ez ON st_contains(ez.geom, t.end_pos_gis)
+	WHERE member_id IN
+	    (SELECT *
+	     FROM fleet_team_id)
+	  AND trip_duration > 0
+	  AND date(t.trip_start_date) >= '2018-01-01'
+
+	   """}
+
+
+
+
+
+
+	   
