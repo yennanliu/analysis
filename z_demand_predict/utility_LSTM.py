b3e0613fa0c2451e6c81b2958ebff12774fb6e51
yennanliue <f339339@gmail.com>
2018-02-13 07:03:38 +0000

    add reshape dataset with Time Steps

--- z_demand_predict/utility_LSTM.py
+++ z_demand_predict/utility_LSTM.py
@@ -20,6 +20,16 @@ from sklearn.metrics import mean_squared_error
 # data preprocess 
 # ----------------
 
+# from dataframe to float32 numpy metrix 
+def prepare_data(df,cols):
+	print (cols)
+	df__ = df_[cols]
+	dataset = df__.values
+	dataset = dataset.astype('float32')
+	return dataset
+
+
+
 # credit 
 # https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
 # convert an array of values into a dataset matrix
@@ -33,6 +43,29 @@ def create_dataset(dataset, look_back=1):
 		dataX.append(a)
 		dataY.append(dataset[i + look_back, 0])
 	return np.array(dataX), np.array(dataY)
+
+
+def reshape_dataset(data_,time_series=False):
+	if time_series=False:
+		"""
+		reshape input to be [samples, time steps, features]
+		e.g.
+		trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+		testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+		"""
+		data_reshape = numpy.reshape(data_, (data_.shape[0], 1, data_.shape[1]))
+
+	else:
+		"""
+		reshape input to be [samples, time steps, features]
+		e.g.
+		trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+		testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+		"""
+		data_reshape = numpy.reshape(data_, (data_.shape[0], data_.shape[1], 1))
+
+	return data_reshape
+
 
 
 # model 
