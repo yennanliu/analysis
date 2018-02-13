b55558972b4cf1c6ea0b3508adce5148dfb99176
yennanliue <f339339@gmail.com>
2018-02-13 06:36:34 +0000

    backup help function fro LSTM

---
+++ z_demand_predict/utility_LSTM.py
@@ -0,0 +1,41 @@
+# ops 
+import pandas as pd 
+import numpy as np
+import datetime
+import os
+
+
+# ML
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.recurrent import LSTM, GRU
+from keras.layers import Convolution1D, MaxPooling1D
+from keras.callbacks import Callback
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+
+
+# data preprocess 
+# ----------------
+
+# credit 
+# https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+
+# model 
+# ----------------
+
+
+
+
+
