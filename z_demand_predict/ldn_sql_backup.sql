28266262b298e9548b773206053886e5fc5350ad
yennanliue <f339339@gmail.com>
2018-03-01 14:59:41 +0000

    fix typo

--- z_demand_predict/ldn_sql_backup.sql
+++ z_demand_predict/ldn_sql_backup.sql
@@ -14,18 +14,31 @@ doc :
 
 -- day total searching 
 
-SELECT substring(start_time_utc,
-        1,
-        10),
+SELECT cast(substring(search_time_utc,
+         1,
+         10) AS date),
          count(*)
 FROM vehicle_search_responses
-WHERE start_time_utc is NOT null
+WHERE search_time_utc is NOT null
 GROUP BY  1
 ORDER BY  1 limit 1000 
 
 
 
--- dev 
+-- day total searching  with specific periods  
+
+SELECT cast(substring(search_time_utc,
+         1,
+         10) AS date),
+         count(*)
+FROM vehicle_search_responses
+WHERE search_time_utc is NOT null
+        AND cast(substring(search_time_utc, 1, 10) AS date) > cast('2018-03-01' AS date)
+GROUP BY  1
+ORDER BY  1  limit 100
+
+
+
 
 
 
