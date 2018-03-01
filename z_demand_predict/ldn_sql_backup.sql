adf39788b65e1344ced7f6f8d0943017457380eb
yennanliue <f339339@gmail.com>
2018-03-01 22:41:04 +0000

    add area filter sql

--- z_demand_predict/ldn_sql_backup.sql
+++ z_demand_predict/ldn_sql_backup.sql
@@ -40,11 +40,19 @@ ORDER BY  1  limit 100
 
 
 
+-- day total searching  with specific periods and ldn area 
 
 
-
-
-
+SELECT cast(substring(search_time_utc, 1, 10) AS date),
+       count(*)
+FROM vehicle_search_responses
+WHERE search_time_utc IS NOT NULL
+  AND cast(substring(search_time_utc, 1, 10) AS date) > cast('2017-01-01' AS date)
+  AND cast(search_lat AS integer) > 50
+  AND cast(search_lat AS integer) < 52
+GROUP BY 1
+ORDER BY 1
+LIMIT 3000
 
 
 
