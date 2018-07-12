27a28d1e45d330b9952f537bc19f7d8c5857dcf2
yennanliue <f339339@gmail.com>
2018-07-12 19:42:26 +0100

    initial commit on r_project

---
+++ project/Rev/SQL/exchange_rates.sql
@@ -0,0 +1,35 @@
+-- exchange_rates
+
+drop table if exists exchange_rates;
+create table exchange_rates(
+ts timestamp without time zone,
+from_currency varchar(3),
+to_currency varchar(3),
+rate numeric
+);
+
+truncate table exchange_rates;
+insert into exchange_rates
+values
+('2018-04-01 00:00:00', 'USD', 'GBP', '0.71'),
+('2018-04-01 00:00:05', 'USD', 'GBP', '0.82'),
+('2018-04-01 00:01:00', 'USD', 'GBP', '0.92'),
+('2018-04-01 01:02:00', 'USD', 'GBP', '0.62'),
+
+('2018-04-01 02:00:00', 'USD', 'GBP', '0.71'),
+('2018-04-01 03:00:05', 'USD', 'GBP', '0.82'),
+('2018-04-01 04:01:00', 'USD', 'GBP', '0.92'),
+('2018-04-01 04:22:00', 'USD', 'GBP', '0.62'),
+
+('2018-04-01 00:00:00', 'EUR', 'GBP', '1.71'),
+('2018-04-01 01:00:05', 'EUR', 'GBP', '1.82'),
+('2018-04-01 01:01:00', 'EUR', 'GBP', '1.92'),
+('2018-04-01 01:02:00', 'EUR', 'GBP', '1.62'),
+
+('2018-04-01 02:00:00', 'EUR', 'GBP', '1.71'),
+('2018-04-01 03:00:05', 'EUR', 'GBP', '1.82'),
+('2018-04-01 04:01:00', 'EUR', 'GBP', '1.92'),
+('2018-04-01 05:22:00', 'EUR', 'GBP', '1.62'),
+
+('2018-04-01 05:22:00', 'EUR', 'HUF', '0.062')
+;
---
+++ project/Rev/SQL/transactions.sql
@@ -0,0 +1,29 @@
+
+-- Transactions
+
+drop table if exists transactions;
+create table transactions (
+ts timestamp without time zone,
+user_id int,
+currency varchar(3),
+amount numeric
+);
+
+truncate table transactions;
+insert into transactions
+values
+('2018-04-01 00:00:00', 1, 'EUR', 2.45),
+('2018-04-01 01:00:00', 1, 'EUR', 8.45),
+('2018-04-01 01:30:00', 1, 'USD', 3.5),
+('2018-04-01 20:00:00', 1, 'EUR', 2.45),
+
+('2018-04-01 00:30:00', 2, 'USD', 2.45),
+('2018-04-01 01:20:00', 2, 'USD', 0.45),
+('2018-04-01 01:40:00', 2, 'USD', 33.5),
+('2018-04-01 18:00:00', 2, 'EUR', 12.45),
+
+('2018-04-01 18:01:00', 3, 'GBP', 2),
+
+('2018-04-01 00:01:00', 4, 'USD', 2),
+('2018-04-01 00:01:00', 4, 'GBP', 2)
+;
