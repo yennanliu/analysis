7b50d35bf5e02ef589753627ec5530d1c928e8af
yennanliue <f339339@gmail.com>
2018-07-20 09:53:11 +0100

    fix all sql and output

--- project/Rev/SQL/GBP_spend_user_latest_exchange_rate.sql
+++ project/Rev/SQL/GBP_spend_user_latest_exchange_rate.sql
@@ -14,66 +14,41 @@ then inner join transaction table with above CTE
 on same from_currency and timestamp of  exchange rate < = timestamp of transaction
 */
 
-/*
-
-
 WITH exchange_ts AS
   (SELECT ts,
-        from_currency,
-        to_currency,
-        rate 
+          from_currency,
+          to_currency,
+          rate
    FROM exchange_rates
    WHERE to_currency = 'GBP'),
-        trans_GBP AS
+     exchange_ts_ AS
+  (SELECT *,
+          lag(ts, -1, '2019-01-01T00:00:00Z') OVER (PARTITION BY from_currency,
+                                                                 to_currency
+                                                    ORDER BY ts) AS ts_lag
+   FROM exchange_rates
+   WHERE to_currency = 'GBP' ),
+     trans_GBP AS
   (SELECT t.user_id AS user_id,
           t.ts AS ts,
           t.currency AS currency,
           t.amount*e.rate AS amount_GBP
    FROM transactions t
-   INNER JOIN exchange_ts e
-   on e.from_currency = t.currency
-   where e.ts <= t.ts )
-SELECT *
-FROM trans_exchange
-ORDER BY user_id,
-         ts
-
-
-*/ 
-
-
-
-
-WITH exchange_ts AS
-  (SELECT ts,
-        from_currency,
-        to_currency,
-        rate 
-   FROM exchange_rates
-   WHERE to_currency = 'GBP'),
-  exchange_ts_  as 
- ( select 
-*, 
-lag(ts,-1 , '2019-01-01T00:00:00Z') over  (partition by from_currency, to_currency  order by ts) as ts_lag
-from exchange_rates WHERE to_currency = 'GBP' ), 
-        trans_GBP AS
-  (SELECT t.user_id AS user_id,
-          t.ts AS ts,
-          t.ts AS t_ts,
-          e.ts as e_ts,
-          e.ts_lag as e_ts_lag,
-          t.currency AS currency,
-          e.rate, 
-          t.amount*e.rate AS amount_GBP,
-          t.amount as amount
-   FROM transactions t
-   INNER JOIN exchange_ts_ e
-   on e.from_currency = t.currency
-   and (  t.ts  >=  e.ts  
-   and e.ts_lag > t.ts   )
-   )
+   INNER JOIN exchange_ts_ e ON e.from_currency = t.currency
+   AND (t.ts >= e.ts
+        AND e.ts_lag > t.ts) ),
+     trans_in_GBP AS
+  (SELECT user_id,
+          ts,
+          currency,
+          amount AS amount_gbp
+   FROM transactions
+   WHERE currency = 'GBP' )
 SELECT *
 FROM trans_GBP
+UNION ALL
+SELECT *
+FROM trans_in_GBP
 ORDER BY user_id,
          ts
 
