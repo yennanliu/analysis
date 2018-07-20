07b9039b6df6ad12c0d9d8975270c980df2af375
yennanliue <f339339@gmail.com>
2018-07-20 09:39:39 +0100

    fix main window exchange logic

--- project/Rev/SQL/GBP_spend_user_latest_exchange_rate.sql
+++ project/Rev/SQL/GBP_spend_user_latest_exchange_rate.sql
@@ -43,32 +43,34 @@ ORDER BY user_id,
 
 
 
+
 WITH exchange_ts AS
   (SELECT ts,
-  		  from_currency,
-  		  to_currency,
-  		  rate 
+        from_currency,
+        to_currency,
+        rate 
    FROM exchange_rates
    WHERE to_currency = 'GBP'),
   exchange_ts_  as 
  ( select 
 *, 
--- if ts_lag = null, replace with  '2018-03-01T00:00:00Z'
-lag(ts,1 , '2018-03-01T00:00:00Z') over  (partition by from_currency, to_currency  order by ts) as ts_lag
+lag(ts,-1 , '2019-01-01T00:00:00Z') over  (partition by from_currency, to_currency  order by ts) as ts_lag
 from exchange_rates WHERE to_currency = 'GBP' ), 
         trans_GBP AS
   (SELECT t.user_id AS user_id,
           t.ts AS ts,
+          t.ts AS t_ts,
+          e.ts as e_ts,
+          e.ts_lag as e_ts_lag,
           t.currency AS currency,
+          e.rate, 
           t.amount*e.rate AS amount_GBP,
           t.amount as amount
    FROM transactions t
    INNER JOIN exchange_ts_ e
    on e.from_currency = t.currency
-   and (  t.ts  <=  e.ts  
-   and e.ts_lag <= t.ts   )
-   or 
-   (e.ts_lag <= t.ts ) 
+   and (  t.ts  >=  e.ts  
+   and e.ts_lag > t.ts   )
    )
 SELECT *
 FROM trans_GBP
