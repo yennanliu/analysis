5f7806c1703f1b468024fe2d979dfd08340248d3
yennanliue <f339339@gmail.com>
2018-10-02 06:23:43 +0100

    update creds directory

--- project/g_image_ML/utility.py
+++ project/g_image_ML/utility.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 #--------------------------------------------------
 # config 
 # save ur google cloud credentials below
-credentials = service_account.Credentials.from_service_account_file('/home/yennanliu/google_cloud_creds2.json')
+credentials = service_account.Credentials.from_service_account_file('/Users/yennanliu/creds/google_cloud_creds2.json')
 client = vision.ImageAnnotatorClient(credentials=credentials)
 print ('client : ', client)
 
