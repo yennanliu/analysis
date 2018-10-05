5088feef0b74e189f69977723f8df76cc5140a31
yennanliue <f339339@gmail.com>
2018-10-05 06:17:40 +0100

    load needed libarary, args, and fix job processes syntax

--- project/g_image_ML/run_multi_process.py
+++ project/g_image_ML/run_multi_process.py
@@ -1,7 +1,9 @@
 # python 3 
 
 import argparse
-import os                                                                       
+import os
+from os import listdir
+from os.path import isfile, join                                                                       
 from multiprocessing import Pool                                                
        
 
@@ -14,26 +16,35 @@ print ('*'*70)
 # ----------------------------------------------
 # get args 
 parser = argparse.ArgumentParser()
+parser.add_argument('--url', required=True, help='The url to load csv')
+parser.add_argument('--script', required=True, help='which script want to run')
 parser.add_argument('--process', required=True, help='How many processes plan to run on the same time')
 args = parser.parse_args()
+
+url  = args.url 
+script  = args.script 
 process  = args.process 
 # ----------------------------------------------
 
 
-
-
-def run_process(process):                                                             
+def run_process(process):
+	# python script.py --csv demp.csv                                                         
 	os.system('python {}'.format(process))                                       
 
-def collect_processes():
+
+def collect_processes(script, url):
+	file_list = [ i for i in listdir(sub_url) if 'sub' in i ]
+	processes = [ '{} --csv {}'.format(script,j)  for j in file_list ]
+	print (' *** processes :  *** ', processes)
 	return processes 
 
-
 def main(processes):
-	processes = ()
 	pool = Pool(processes=processes)                                                        
 	pool.map(run_process, processes)
 
 
 if __name__ == '__main__':
-	main()
+	processes=collect_processes(script,url)
+	main(processes)
+
+
