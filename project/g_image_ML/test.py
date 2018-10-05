5270ec881ea36818795a0ca071cb4a88ba7af6b5
yennanliue <f339339@gmail.com>
2018-10-05 07:51:15 +0100

    fix load args for only load csv once

--- project/g_image_ML/test.py
+++ project/g_image_ML/test.py
@@ -15,19 +15,38 @@ import pandas as pd
 # ----------------------------------------------
 # get args 
 parser = argparse.ArgumentParser()
-parser.add_argument('--url', required=True, help='The url to load csv')
-parser.add_argument('--csv', required=True, help='The csv to load')
+#parser.add_argument('--url', required=True, help='The --csvurl to load')
+parser.add_argument('--csvurl', required=True, help='The --csvurl to load')
+
 args = parser.parse_args()
 
-url  = args.url 
-csv  = args.csv
+#url  = args.url 
+csvurl  = args.csvurl 
+
 # ----------------------------------------------
+
+print (' ***  csvurl : *** ', csvurl)
+df=pd.read_csv( csvurl)
+print (' *** len(df) : *** ', len(df))
+print (df.head(2))
+
+"""
+sub_url = '/' + '/'.join([ i for i in url.split('/')[1:-1]]) + '/'  
+file_list = [ i for i in listdir(sub_url) if 'sub' in i ]
+csvurl =  [ sub_url + i for i in listdir(sub_url) if 'sub' in i ]
+print (' ***  csvurl : *** ', csvurl)
+df=pd.read_csv( csvurl)
+print (' *** len(df) : *** ', len(df))
+print (df.head(2))
+"""
+
+"""
 sub_url = '/' + '/'.join([ i for i in url.split('/')[1:-1]]) + '/'  
 for sub in [ i for i in listdir(sub_url) if 'sub' in i ]:
 	print ('sub : ', sub)
 	df=pd.read_csv( sub_url + sub)
 	print (' *** len(df) : *** ', len(df))
 	print (df.head(2))
-
+"""
 
 
