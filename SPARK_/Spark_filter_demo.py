fe92a907856f1028e891b9e0b6133278d4a6018f
yennanliu <f339339@gmail.com>
2018-11-28 11:47:47 +0000

    rename and backup code

--- SPARK_/Spark_filter_demo.py
+++ SPARK_/Spark_filter_demo.py
@@ -1,20 +1,22 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
+import os 
 
 conf = SparkConf().setAppName("building a warehouse")
 sc = SparkContext(conf=conf)
 sqlCtx = SQLContext(sc)
 
 
+#logFile = "YOUR_SPARK_HOME/README.md"  # Should be some file on your system
+spark_home = os.environ.get('SPARK_HOME', None)
+logFile = sc.textFile(spark_home + "/README.md")
 
-logFile = "YOUR_SPARK_HOME/README.md"  # Should be some file on your system
-spark = SparkSession.builder().appName(appName).master(master).getOrCreate()
-logData = spark.read.text(logFile).cache()
+logData = sc.read.text(logFile).cache()
 
 numAs = logData.filter(logData.value.contains('a')).count()
 numBs = logData.filter(logData.value.contains('b')).count()
 
 print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
 
-spark.stop()
+sc.stop()
