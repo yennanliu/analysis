448f9d6e5abf04fa568cf71f450ea57e4290280d
yennanliue <f339339@gmail.com>
2018-12-04 01:07:43 +0000

    refactor code, make funcs working ops

--- SPARK_/Spark_PTTdata_Mysql_analysis.py
+++ SPARK_/Spark_PTTdata_Mysql_analysis.py
@@ -19,11 +19,12 @@ import os
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 
-conf = SparkConf().setAppName("LOAD MYSQL DATABASE")
+# spark config 
+conf = SparkConf().setAppName("LOAD PTT MYSQL DATABASE")
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
-
+# mysql 
 HOST = os.environ['HOST']
 USER = os.environ['USER']
 PASSWORD = os.environ['PASSWORD']
@@ -31,27 +32,37 @@ DATABASE = os.environ['DATABASE']
 
 
 
-url="jdbc:mysql://{}/{}".format(HOST, DATABASE)
-# For SQLServer, pass in the "driver" option
-# driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-# Add "driver" : driverClass
-connectionProperties = {
-  "user" : USER,
-  "password" : PASSWORD
-}
-table= "Soft_Job"
-spark_df = sqlContext.read.jdbc(url=url, table=table , properties=connectionProperties)
-pandas_df = spark_df.toPandas()
-#sqlContext=SQLContext(sc)
-#df=sqlContext.read.jdbc(url=url, table=pushdown_query, properties=properties)
-print ('='*70)
-print ('spark_df : ', spark_df.take(40))
-print (type(spark_df))
-print ('pandas_df : ', pandas_df.head(40))
-print (type(pandas_df))
-print ('='*70)
+def get_mysql_creds():
+	url = "jdbc:mysql://{}/{}".format(HOST, DATABASE)
+	creds = {"url":url,
+			 "connectionProperties" : {
+			 "user" : USER,
+			 "password" : PASSWORD
+				}}
+	return creds 
 
-##### run via command line #####   
-# spark-submit --packages mysql:mysql-connector-java:5.1.38 Spark_load_MySQL_demo.py
+
+def get_ptt_table_data(creds, table):
+	creds = get_mysql_creds()
+	spark_df = sqlContext.read.jdbc(url=creds['url'],properties=creds['connectionProperties'], table=table )
+	pandas_df = spark_df.toPandas()
+	return spark_df, pandas_df 
+
+
+if __name__ == '__main__':
+	creds = get_mysql_creds()
+	spark_df, pandas_df  = get_ptt_table_data(creds, "Soft_Job")
+	print ('='*70)
+	print ('spark_df : ', spark_df.take(40))
+	print (type(spark_df))
+	print ('pandas_df : ', pandas_df.head(40))
+	print (type(pandas_df))
+	print ('='*70)
+	##### run via command line #####   
+	# spark-submit --packages mysql:mysql-connector-java:5.1.38 Spark_load_MySQL_demo.py
+
+
+
+
 
 
