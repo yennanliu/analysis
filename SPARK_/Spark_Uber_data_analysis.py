eda16f91e4575bf1ed0a7f66c5ecba690243c217
yennanliue <f339339@gmail.com>
2019-03-16 13:53:33 +0000

    add filter/groupby func

--- SPARK_/Spark_Uber_data_analysis.py
+++ SPARK_/Spark_Uber_data_analysis.py
@@ -12,7 +12,6 @@ sqlCtx = SQLContext(sc)
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
 
-
 def load_csv():
 	# load the csv data as spark df 
 	data = sc.textFile("uber_data/Uber-Jan-Feb-FOIL.csv").map(lambda line: line.split(","))
@@ -20,23 +19,35 @@ def load_csv():
 	traindata = data.filter(lambda row: row != headers)
 	sqlContext = SQLContext(sc)
 	dataFrame = sqlContext.createDataFrame(traindata, ['dispatching_base_number', 'date', 'active_vehicles', 'trips'])
+	dataFrame = dataFrame.withColumn("date_", dataFrame["date"].cast("timestamp"))
+	dataFrame = dataFrame.withColumn("trips", dataFrame["trips"].cast("float"))
+	dataFrame = dataFrame.withColumn("active_vehicles", dataFrame["active_vehicles"].cast("float"))
 	dataFrame.show()
+	print (dataFrame.printSchema())
 	return dataFrame
 
 
 def filter_batch_drive(dataFrame):
 	spark_RDD = dataFrame.rdd
 	digested_RDD = spark_RDD.map(
-					lambda x: Row(
-					author_ip = x['trips'],
-					timestamp = x['date']))\
-				.take(30)
+	lambda x: Row(
+		trips = x['trips'],
+		dispatching_number = x['dispatching_base_number'],
+		timestamp = x['date']))\
+		.filter(lambda x : x['dispatching_number'] == 'B02765')\
+		.take(30)
 	print (digested_RDD) 
+
+def group_by_batch_drive(dataFrame):
+	dataFrame.groupby('dispatching_base_number').count().show()
+	dataFrame.groupby('dispatching_base_number').sum().show()
+
 
 
 if __name__ == '__main__':
 	dataFrame = load_csv()
 	filter_batch_drive(dataFrame)
+	group_by_batch_drive(dataFrame)
 
 
 
