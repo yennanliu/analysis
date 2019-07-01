00a386d91696c0268ed1a02d7203dc414d9431ec
yennanliue <f339339@gmail.com>
2019-07-02 00:55:56 +0800

    backup code

--- DS_algorithms/map_reduce.py
+++ DS_algorithms/map_reduce.py
@@ -1,131 +1,44 @@
-# modify from 
-# https://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/
+import csv 
 
-#################################################################
-# V1 :  mapper.py 
-#################################################################
-#!/usr/bin/env python
-"""mapper.py"""
+class map_reduce(object):
 
-import sys
-# input comes from STDIN (standard input)
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
+	def __init__(self):
+		pass 
 
+	def load_data(self, dataset):
+		agg_row = []
+		with open(dataset) as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=',')
+			for row in csv_reader:
+				print (row)
+				agg_row.append(row[0])
+		return agg_row 
 
-#################################################################
-# V1 :  reducer.py 
-#################################################################
-#!/usr/bin/env python
-"""reducer.py"""
+	def map(self, agg_row):
+		print (agg_row)
+		for row in agg_row:
+			print ('%s\t%s' % (row, 1))
 
-from operator import itemgetter
-import sys
+	def reduce(self, agg_row):
+		current_word = None
+		current_count = 0 
+		word = None 
+		for row in agg_row:
+			#word, count = row.split('\t', 1)
+			word, count = row.split('\t', 1)[0], 1 
+			print (word, count)
+			if word == current_word:
+				current_count = current_count + count 
+			else:
+				if current_word:
+					print ('%s\t%s' % (current_word, current_count))
+			current_count = count 
+			current_word = word 
+		if current_word == word:
+			print ('%s\t%s' % (current_word, current_count))
 
-current_word = None
-current_count = 0
-word = None
-
-# input comes from STDIN
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-
-    # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
-
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
-
-# do not forget to output the last word if needed!
-if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
-
-
-#################################################################
-# V2 :  mapper.py  : via python iterators and generators 
-#################################################################
-#!/usr/bin/env python
-"""A more advanced Mapper, using Python iterators and generators."""
-
-import sys
-
-def read_input(file):
-    for line in file:
-        # split the line into words
-        yield line.split()
-
-def main(separator='\t'):
-    # input comes from STDIN (standard input)
-    data = read_input(sys.stdin)
-    for words in data:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        for word in words:
-            print '%s%s%d' % (word, separator, 1)
-
-# if __name__ == "__main__":
-#     main()
-
-
-#################################################################
-# V2 :  reducer.py  : via python iterators and generators 
-#################################################################
-#!/usr/bin/env python
-"""A more advanced Reducer, using Python iterators and generators."""
-
-from itertools import groupby
-from operator import itemgetter
-import sys
-
-def read_mapper_output(file, separator='\t'):
-    for line in file:
-        yield line.rstrip().split(separator, 1)
-
-def main(separator='\t'):
-    # input comes from STDIN (standard input)
-    data = read_mapper_output(sys.stdin, separator=separator)
-    # groupby groups multiple word-count pairs by word,
-    # and creates an iterator that returns consecutive keys and their group:
-    #   current_word - string containing a word (the key)
-    #   group - iterator yielding all ["&lt;current_word&gt;", "&lt;count&gt;"] items
-    for current_word, group in groupby(data, itemgetter(0)):
-        try:
-            total_count = sum(int(count) for current_word, count in group)
-            print "%s%s%d" % (current_word, separator, total_count)
-        except ValueError:
-            # count was not a number, so silently discard this item
-            pass
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == '__main__':
+	mapreduce = map_reduce()
+	agg_row = mapreduce.load_data('map_reduce.csv')
+	#mapreduce.map(agg_row)
+	mapreduce.reduce(agg_row)
