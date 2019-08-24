b0bc5ec87cb5e1123651ae23c1a89d63b9cdae13
yennanliu <f339339@gmail.com>
2019-08-24 22:28:44 +0800

    encode kafka msg key, value to make kafka consumer be able to receive the stream data and remove no need func

--- SPARK_/kafka_spark_stream/s3_csv_2_stream.py
+++ SPARK_/kafka_spark_stream/s3_csv_2_stream.py
@@ -2,33 +2,18 @@ import time
 import json
 import boto3
 import lazyreader
-#import utility
 from kafka.producer import KafkaProducer
-
-def get_key(self, msg):
-    """
-    produces key for message to Kafka topic
-    :type msg: dict     message for which to generate the key
-    :rtype   : str      key that has to be of type bytes
-    """
-    msgwithkey = utility.add_block_fields(msg)
-    if msgwithkey is None:
-        return
-    x, y = msgwithkey["block_lonid"], msgwithkey["block_latid"]
-    return str((x*137+y)%77703).encode()
-
 
 if __name__ == '__main__':
     s3 = boto3.client('s3')
-    producer = KafkaProducer(bootstrap_servers="localhost:9092")
+    producer = KafkaProducer(bootstrap_servers="127.0.0.1:9092")
     obj = s3.get_object(Bucket='nyctaxitrip',
             Key="{}/{}".format('yellow_trip',
                                'yellow_tripdata_sample.csv'))
     lines = str(obj['Body'].read())
     for line in lines.split("\\n"):
-        #print (line)
         print (json.dumps(line))
         producer.send("new_topic",
-                      value=json.dumps(line),
-                      key='key')
+                      value=line.encode(),    # encode the value to enable kafka consumer to recieve the stream msg
+                      key=b'key')             # encode the key to enable kafka consumer to recieve the stream msg
         time.sleep(0.1)
