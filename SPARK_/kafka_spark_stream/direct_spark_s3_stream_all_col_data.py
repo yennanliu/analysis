e2210258a7e6f03cc43ad956d064ac5269e5afc4
yennanliu <f339339@gmail.com>
2019-10-01 09:10:09 +0800

    rename archive -> archived

--- SPARK_/kafka_spark_stream/direct_spark_s3_stream_all_col_data.py
+++ SPARK_/kafka_spark_stream/direct_spark_s3_stream_all_col_data.py
@@ -5,6 +5,14 @@ from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql.session import SparkSession
 from pyspark.sql import Row
 from pyspark.ml.linalg import Vectors
+import pygeohash as pgh
+from pyspark.sql import functions as F
+
+def get_geohash_id(taxidf):
+    udf_geohash = F.udf(lambda x,y: pgh.encode(x,y,precision=7))
+    pickup_geohash = taxidf.select('Trip_Pickup_DateTime','Start_Lat','Start_Lon',udf_geohash('Start_Lat','Start_Lon').alias('geo_hash_id'))
+    dropoff_geohash = taxidf.select('Trip_Dropoff_DateTime','End_Lat','End_Lon',udf_geohash('End_Lat','End_Lon').alias('geo_hash_id'))
+    return pickup_geohash, dropoff_geohash
 
 def getSparkSessionInstance(sparkConf):
     if ('sparkSessionSingletonInstance' not in globals()):
@@ -14,7 +22,7 @@ def getSparkSessionInstance(sparkConf):
             .getOrCreate()
     return globals()['sparkSessionSingletonInstance']
 
-def stream_2_sql(time, rdd):
+def stream_2_sql_2_df(time, rdd):
     """
     https://spark.apache.org/docs/2.1.1/streaming-programming-guide.html
     https://github.com/apache/spark/blob/v2.1.1/examples/src/main/python/streaming/sql_network_wordcount.py
@@ -63,7 +71,28 @@ def stream_2_sql(time, rdd):
                 'End_Lon','End_Lat','Payment_Type',
                 'Fare_Amt','surcharge','mta_tax',
                 'Tip_Amt','Tolls_Amt','Total_Amt'])
+        taxidf = taxidf.withColumn("vendor_name", taxidf["vendor_name"].cast("string"))    
+        taxidf = taxidf.withColumn("Trip_Pickup_DateTime", taxidf["Trip_Pickup_DateTime"].cast("timestamp"))
+        taxidf = taxidf.withColumn("Trip_Dropoff_DateTime", taxidf["Trip_Dropoff_DateTime"].cast("timestamp"))
+        taxidf = taxidf.withColumn("Passenger_Count", taxidf["Passenger_Count"].cast("integer"))
+        taxidf = taxidf.withColumn("Trip_Distance", taxidf["Trip_Distance"].cast("float"))
+        taxidf = taxidf.withColumn("Start_Lon", taxidf["Start_Lon"].cast("float"))
+        taxidf = taxidf.withColumn("Start_Lat", taxidf["Start_Lat"].cast("float"))
+        taxidf = taxidf.withColumn("Rate_Code", taxidf["Rate_Code"].cast("string"))
+        taxidf = taxidf.withColumn("store_and_forward", taxidf["store_and_forward"].cast("string"))
+        taxidf = taxidf.withColumn("End_Lon", taxidf["End_Lon"].cast("float"))
+        taxidf = taxidf.withColumn("End_Lat", taxidf["End_Lat"].cast("float"))
+        taxidf = taxidf.withColumn("Payment_Type", taxidf["Payment_Type"].cast("string"))
+        taxidf = taxidf.withColumn("Fare_Amt", taxidf["Fare_Amt"].cast("float"))
+        taxidf = taxidf.withColumn("surcharge", taxidf["surcharge"].cast("float"))
+        taxidf = taxidf.withColumn("mta_tax", taxidf["mta_tax"].cast("float"))
+        taxidf = taxidf.withColumn("Tip_Amt", taxidf["Tip_Amt"].cast("float"))
+        taxidf = taxidf.withColumn("Tolls_Amt", taxidf["Tolls_Amt"].cast("float"))
+        taxidf = taxidf.withColumn("Total_Amt", taxidf["Total_Amt"].cast("float"))
         print('taxidf :', taxidf.collect())
+        pickup_geohash, dropoff_geohash = get_geohash_id(taxidf)
+        print('pickup_geohash :', pickup_geohash.collect())
+        print('dropoff_geohash :', dropoff_geohash.collect())
         #return taxidf 
     except Exception as e:
         print ( str(e), 'sth goes wrong')
@@ -81,6 +110,6 @@ if __name__ == "__main__":
 
     lines = kafkaStream.map(lambda x: x[1])
     lines_ = lines.flatMap(lambda line: line.split("\n"))
-    lines_.foreachRDD(stream_2_sql)
+    lines_.foreachRDD(stream_2_sql_2_df)
     ssc.start()
     ssc.awaitTermination()
