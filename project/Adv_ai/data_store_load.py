8c7ed0d1b47545e46ae3d5cd6f445fe42811937d
yennanliu <f339339@gmail.com>
2019-10-14 14:18:57 +0800

    fix layout, naming and to_load logic

--- project/Adv_ai/data_store_load.py
+++ project/Adv_ai/data_store_load.py
@@ -1,23 +1,29 @@
 class data_store_load:
+    """
+    : input  : array of dict
+      e.g. : [{"a": 1, "b":2}, {"c":3}, {"d":4, "e":5}]
+
+    : output : str
+      e.g. : 'a=1;b=2;\nc=3;\nd=4;e=5;\n'
+    """
 
     def __init__(self):
         self.storage = ""
 
-    def my_storage(self, my_array):
-        storage = ""
+    def to_storage(self, my_array):
         for sub_dict in my_array:
             print (sub_dict)
             if type(sub_dict) == dict:
                 for key in sub_dict:
-                    storage += str(key)
-                    storage += "="
-                    storage += str(sub_dict[key])
-                    storage += ";"
-                storage += "\n"
+                    self.storage += str(key)
+                    self.storage += "="
+                    self.storage += str(sub_dict[key])
+                    self.storage += ";"
+                self.storage += "\n"
+        print (self.storage)
+        return self.storage
+
+    def to_load(self):
+        storage = self.storage
         print (storage)
         return storage
-
-
-    def my_load(self):
-        storage = storage
-   
