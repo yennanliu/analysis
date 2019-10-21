2db99523caaec88098ab42cb744247ebbe4f8a04
yennanliu <f339339@gmail.com>
2019-10-21 22:44:21 +0800

    add comment/ dump to db to etl script

--- project/warg_/etl.py
+++ project/warg_/etl.py
@@ -1,14 +1,18 @@
 import json 
 import pandas as pd 
-
-
+from sqlalchemy import create_engine
 
 def main():
+    """
+    main etl transform json -> sqlite db data
+    """
     data_battle = []
     data_session = []
     data_cost = []
+    print ('>>>> load json data')
     with open('data/war_data.json') as json_file:
         json_data = json.load(json_file)
+    print ('>>>> extract json data')
     for i in range(len(json_data)):
         tmp_json_keys = list(json_data[i].keys())
         if 'battle_id' in tmp_json_keys:
@@ -19,12 +23,17 @@ def main():
             data_cost.append(json_data[i])
         else:
             print ('no defined event format')
+    # to csv 
+    print ('>>>> json to csv')
     df_battle = pd.DataFrame(data_battle)
     df_session = pd.DataFrame(data_session)
     df_cost = pd.DataFrame(data_cost)
-    print (df_battle.head())
-    print (df_session.head())
-    print (df_session.head())
+    # to db 
+    print ('>>>> json to db')
+    engine = create_engine('sqlite:///data/wargame.db', echo=False)
+    df_battle.to_sql(name='battle', con= engine)
+    df_session.to_sql(name='session', con= engine)
+    df_cost.to_sql(name='cost', con= engine)
 
 if __name__ == '__main__':
     main()
