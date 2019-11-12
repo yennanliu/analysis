238ff284a24e30bc4fee266799efe72eea32e1a0
yennanliu <f339339@gmail.com>
2019-11-12 11:40:20 +0800

    add sql

---
+++ project/ci/athena/vm_list_5271.sql
@@ -0,0 +1,15 @@
+CREATE EXTERNAL TABLE `vm_list_5271`(
+  `index` string, 
+  `vm_id` string)
+ROW FORMAT DELIMITED 
+  FIELDS TERMINATED BY ',' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://suntory-data/vm_list_5271'
+TBLPROPERTIES (
+  'has_encrypted_data'='false', 
+  'skip.header.line.count'='1', 
+  'transient_lastDdlTime'='1573529963')
