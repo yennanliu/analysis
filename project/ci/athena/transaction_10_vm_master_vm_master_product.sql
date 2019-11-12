236d8f98464a1503c3ea53249234abe93bfde153
yennanliu <f339339@gmail.com>
2019-11-12 16:41:15 +0800

    fix sql

--- project/ci/athena/transaction_10_vm_master_vm_master_product.sql
+++ project/ci/athena/transaction_10_vm_master_vm_master_product.sql
@@ -3,11 +3,10 @@ t.*,
 v.*,
 p.*
 FROM "suntory"."transaction_10_vm" as t 
-left join 
+inner join 
 "suntory"."master_vm" as v
-on v.equipment_code  =  t.equipment_code
-and v.column_no  =  t.column_no
-left join 
+on v.equipment_code  =  t.equipment_code 
+and v.column_no  =  t.column_no 
+inner join 
 "suntory"."master_product" as p
-on t.product_code  =  p.product_code
-;
+on t.product_code  =  p.product_code;
