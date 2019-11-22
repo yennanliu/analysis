aed064af8d5b2db45635d781a1c1e65aa641c37d
yennanliu <f339339@gmail.com>
2019-11-22 12:03:20 +0800

    update sql

--- project/ci/redshift/adhoc/top_prod.sql
+++ project/ci/redshift/adhoc/top_prod.sql
@@ -12,8 +12,8 @@ WITH trans AS
    UNION ALL SELECT *
    FROM public.filtered_9000_vm_transaction_201906)
 SELECT product_code,
-       sum(sales_quantity),
-       count(equipment_code)
+       sum(sales_quantity) as sum_sales_quantity,
+       count(equipment_code) as count_vm
 FROM trans
 GROUP BY 1
 ORDER BY 2 DESC;
