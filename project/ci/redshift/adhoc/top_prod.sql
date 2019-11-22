984b90dd4d11a2123375fdfaa2a7f7e5e625477c
yennanliu <f339339@gmail.com>
2019-11-22 11:57:40 +0800

    add sql

---
+++ project/ci/redshift/adhoc/top_prod.sql
@@ -0,0 +1,20 @@
+WITH trans AS
+  (SELECT *
+   FROM public.filtered_9000_vm_transaction_201901
+   UNION ALL SELECT *
+   FROM public.filtered_9000_vm_transaction_201902
+   UNION ALL SELECT *
+   FROM public.filtered_9000_vm_transaction_201903
+   UNION ALL SELECT *
+   FROM public.filtered_9000_vm_transaction_201904
+   UNION ALL SELECT *
+   FROM public.filtered_9000_vm_transaction_201905
+   UNION ALL SELECT *
+   FROM public.filtered_9000_vm_transaction_201906)
+SELECT product_code,
+       sum(sales_quantity),
+       count(equipment_code)
+FROM trans
+GROUP BY 1
+ORDER BY 2 DESC;
+
