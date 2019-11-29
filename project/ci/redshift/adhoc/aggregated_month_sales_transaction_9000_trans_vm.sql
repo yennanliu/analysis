85b9d9bc7d98ebff5d5be97eb3e63f33d52c503b
yennanliu <f339339@gmail.com>
2019-11-29 15:36:19 +0800

    add/update sql/script

--- project/ci/redshift/adhoc/aggregated_month_sales_transaction_9000_trans_vm.sql
+++ project/ci/redshift/adhoc/aggregated_month_sales_transaction_9000_trans_vm.sql
@@ -104,22 +104,129 @@
 -- V3
 -----------------------------------
 
+-- WITH filtered_vm AS
+--   (SELECT sales_date,
+--           branch_number,
+--           customer_number,
+--           equipment_code,
+--           count(DISTINCT product_code)::numeric/max(column_no)::numeric AS item_ratio
+--    FROM target_vm_9000_vm_transaction_201812
+--    GROUP BY 1,
+--             2,
+--             3,
+--             4
+--    HAVING item_ratio > 0.5),
+--      filtered_t AS
+--   (SELECT *
+--    FROM target_vm_9000_vm_transaction_201812
+--    WHERE (sales_date,
+--           branch_number,
+--           customer_number,
+--           equipment_code) IN
+--        (SELECT sales_date,
+--                branch_number,
+--                customer_number,
+--                equipment_code
+--         FROM filtered_vm) ),
+--      aggre_t AS
+--   (SELECT SUBSTRING(sales_date, 1, 7) AS sales_month,
+--           branch_number,
+--           customer_number,
+--           equipment_code,
+--           column_no,
+--           product_code,
+--           site_code,
+--           department_code,
+--           route_code,
+--           customer_number,
+--           group_company_code,
+--           selling_price,
+--           meter_no,
+--           hot_cold_classification,
+--           buying_place_code,
+--           sales_system_representative_code,
+--           sales_method_detail_code,
+--           in_out_classification,
+--           annual_contribution_sales_capacity_conversion,
+--           open_closed_classification,
+--           contents_manufacturer_code,
+--           installation_date,
+--           product_code_before_replacement,
+--           hot_cold_classification_before_product_replacement AS hot_cold_classification_before_replacement,
+--           number_of_sales_update_failure,
+--           number_of_adjacent_vm_cc,
+--           number_of_adjacent_vm_k,
+--           number_of_adjacent_vm_a,
+--           number_of_adjacent_vm_dy,
+--           number_of_adjacent_vm_it,
+--           number_of_adjacent_vm_po,
+--           number_of_adjacent_vm_ot,
+--           number_of_adjacent_vm_sf,
+--           sum(sales_quantity) AS sales_quantity
+--    FROM filtered_t
+--    WHERE number_of_sales_update_failure = 0
+--      AND sales_quantity < 1000
+--    GROUP BY 1,
+--             2,
+--             3,
+--             4,
+--             5,
+--             6,
+--             7,
+--             8,
+--             9,
+--             10,
+--             11,
+--             12,
+--             13,
+--             14,
+--             15,
+--             16,
+--             17,
+--             18,
+--             19,
+--             20,
+--             21,
+--             22,
+--             23,
+--             24,
+--             25,
+--             26,
+--             27,
+--             28,
+--             29,
+--             30,
+--             31,
+--             32,
+--             33)
+-- SELECT *
+-- FROM aggre_t;
+
+-----------------------------------
+-- V4
+-----------------------------------
+
 WITH filtered_vm AS
   (SELECT sales_date,
+          nnp_group_company_code,
+          nnp_organization_group_code,
           branch_number,
           customer_number,
-          equipment_code,
-          count(DISTINCT product_code)::numeric/max(column_no)::numeric AS item_ratio
+          equipment_code
    FROM target_vm_9000_vm_transaction_201812
+   WHERE number_of_sales_update_failure = 0
+     AND sales_quantity < 1000
    GROUP BY 1,
             2,
             3,
-            4
-   HAVING item_ratio > 0.5),
+            4,
+            5),
      filtered_t AS
   (SELECT *
    FROM target_vm_9000_vm_transaction_201812
    WHERE (sales_date,
+          nnp_group_company_code,
+          nnp_organization_group_code,
           branch_number,
           customer_number,
           equipment_code) IN
@@ -162,10 +269,9 @@ WITH filtered_vm AS
           number_of_adjacent_vm_po,
           number_of_adjacent_vm_ot,
           number_of_adjacent_vm_sf,
+          count(DISTINCT product_code)::numeric/max(column_no)::numeric AS item_ratio,
           sum(sales_quantity) AS sales_quantity
    FROM filtered_t
-   WHERE number_of_sales_update_failure = 0
-     AND sales_quantity < 1000
    GROUP BY 1,
             2,
             3,
@@ -198,6 +304,7 @@ WITH filtered_vm AS
             30,
             31,
             32,
-            33)
+            33
+      HAVING item_ratio > 0.5)
 SELECT *
 FROM aggre_t;
---
+++ project/ci/redshift/build_aggre_month_transaction.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+####################################################################
+# SCRIPT BUILD AGGREGRATED MONTH TRANSACTION TABLE
+####################################################################
+
+
+build_month_aggr_trans() {
+    base_sql="""
+
+      WITH filtered_vm AS
+        (SELECT sales_date,
+                branch_number,
+                customer_number,
+                equipment_code,
+                count(DISTINCT product_code)::numeric/max(column_no)::numeric AS item_ratio
+         FROM target_vm_9000_vm_transaction_
+         GROUP BY 1,
+                  2,
+                  3,
+                  4
+         HAVING item_ratio > 0.5),
+           filtered_t AS
+        (SELECT *
+         FROM target_vm_9000_vm_transaction_
+         WHERE (sales_date,
+                branch_number,
+                customer_number,
+                equipment_code) IN
+             (SELECT sales_date,
+                     branch_number,
+                     customer_number,
+                     equipment_code
+              FROM filtered_vm) ),
+           aggre_t AS
+        (SELECT SUBSTRING(sales_date, 1, 7) AS sales_month,
+                branch_number,
+                customer_number,
+                equipment_code,
+                column_no,
+                product_code,
+                site_code,
+                department_code,
+                route_code,
+                customer_number,
+                group_company_code,
+                selling_price,
+                meter_no,
+                hot_cold_classification,
+                buying_place_code,
+                sales_system_representative_code,
+                sales_method_detail_code,
+                in_out_classification,
+                annual_contribution_sales_capacity_conversion,
+                open_closed_classification,
+                contents_manufacturer_code,
+                installation_date,
+                product_code_before_replacement,
+                hot_cold_classification_before_product_replacement AS hot_cold_classification_before_replacement,
+                number_of_sales_update_failure,
+                number_of_adjacent_vm_cc,
+                number_of_adjacent_vm_k,
+                number_of_adjacent_vm_a,
+                number_of_adjacent_vm_dy,
+                number_of_adjacent_vm_it,
+                number_of_adjacent_vm_po,
+                number_of_adjacent_vm_ot,
+                number_of_adjacent_vm_sf,
+                sum(sales_quantity) AS sales_quantity
+         FROM filtered_t
+         WHERE number_of_sales_update_failure = 0
+           AND sales_quantity < 1000
+         GROUP BY 1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  12,
+                  13,
+                  14,
+                  15,
+                  16,
+                  17,
+                  18,
+                  19,
+                  20,
+                  21,
+                  22,
+                  23,
+                  24,
+                  25,
+                  26,
+                  27,
+                  28,
+                  29,
+                  30,
+                  31,
+                  32,
+                  33)
+      SELECT *
+      FROM aggre_t;
+       """
+    for i in 201901 201902 201903
+        do
+            SQL=$(echo $base_sql | sed -e "s/\target_vm_9000_vm_transaction_/target_vm_9000_vm_transaction_$i/g")
+            echo $SQL
+            echo --------
+        done 
+}
+
+build_month_aggr_trans
