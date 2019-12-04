a8674b2d9f1031dfd4378f581480d6bffc8c1f2a
yennanliu <f339339@gmail.com>
2019-12-04 09:30:56 +0800

    update sql

---
+++ project/ci/athena/vm_portofolio_201801_201906.sql.py
@@ -0,0 +1,20 @@
+CREATE EXTERNAL TABLE `vm_portofolio_201801_201906`(
+  `group_company_code` string, 
+  `organization_group_code` string, 
+  `customer_number` string, 
+  `branch_number` string, 
+  `equipment_code` string, 
+  `product_code` string, 
+  `sales_date` string)
+ROW FORMAT DELIMITED 
+  FIELDS TERMINATED BY ',' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://suntory-data/Aggregated_VM_month_portofolio'
+TBLPROPERTIES (
+  'has_encrypted_data'='false', 
+  'skip.header.line.count'='1', 
+  'transient_lastDdlTime'='1575359910')
